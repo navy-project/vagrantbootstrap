398df0a661560aed5d1006ebb5a73780432f1a07
Tobias Haar <tobias.haar@gmail.com>
2015-01-20 16:51:51 +0000

    improved start script

--- start.sh
+++ start.sh
@@ -1,40 +1,50 @@
 #!/bin/bash -e
 
-function usage() {
-    echo "Usage: $0 dev|nodev"
-    echo ""
-    echo "Starts all the containers needed to run the integration tests"
-    echo ""
-    echo "Options:"
-    echo "  dev   : the radar container's document root will be /vagrant (allowing you to work from the dir from where you ran vagrant ssh)"
-    echo "  nodev : the radar container's document root will be inside the container (meaning you can't modify the files)"
-    echo ""
-    exit 1
+at() {
+  echo "--" "$@"
+  date +%H:%M:%S.%N &>> start.log
 }
 
-case "$1" in
-  dev)
-    dev=true
-    ;;
-  nodev)
-    dev=false
-    ;;
-  *)
-    usage
-    ;;
-esac
+die() {
+  at "Failure: $@"
+  echo "Check 'start.log' for more information"
+  exit 1
+}
 
-docker run -d --name container_proxy -v /var/run/docker.sock:/tmp/docker.sock -p 172.17.42.1:8080:80 navyproject/container-proxy
-docker run -d --name host_proxy -v /var/run/docker.sock:/tmp/docker.sock -p 443:443 navyproject/host-proxy nginx
-docker run -d --name etcd navyproject/etcd
-docker run -d --name commodore --link=etcd:etcd navyproject/commodore
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock --link=etcd:etcd --name harbourmaster navyproject/harbourmaster
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock --link=etcd:etcd --name coastguard navyproject/coastguard
-docker run -d --link=etcd:etcd --name navyapi -p 4040:4040 navyproject/navyapi
-docker run -d --name navystreamwatcher -p 4041:4041 navyproject/navystreamwatcher
-if [ $dev = true ]
-then
-  docker run -d --name radar -p 8000:8000 -v /vagrant:/var/reactclient navyproject/radar
-else
-  docker run -d --name radar -p 8000:8000 navyproject/radar
-fi
+erl() {
+  at "  ""$@"
+  "$@" &>> start.log
+  return $?
+}
+
+echo "Starting container_proxy"
+erl docker run -d --name container_proxy -v /var/run/docker.sock:/tmp/docker.sock -p 172.17.42.1:8080:80 navyproject/container-proxy || \
+  die "Could not start container_proxy"
+
+echo "Starting host_proxy"
+erl docker run -d --name host_proxy -v /var/run/docker.sock:/tmp/docker.sock -p 443:443 navyproject/host-proxy nginx || \
+  die "Could not start host_proxy"
+
+echo "Starting etcd"
+erl docker run -d --name etcd navyproject/etcd || \
+  die "Could not start etcd"
+
+echo "Starting commodore"
+erl docker run -d --name commodore --link=etcd:etcd navyproject/commodore || \
+  die "Could not start commodore"
+
+echo "Starting harbourmaster"
+erl docker run -d -v /var/run/docker.sock:/var/run/docker.sock --link=etcd:etcd --name harbourmaster navyproject/harbourmaster || \
+  die "Could not start harbourmaster"
+
+echo "Starting coastguard"
+erl docker run -d -v /var/run/docker.sock:/var/run/docker.sock --link=etcd:etcd --name coastguard navyproject/coastguard || \
+  die "Could not start coastguard"
+
+echo "Starting navyapi"
+erl docker run -d --link=etcd:etcd --name navyapi -p 4040:4040 navyproject/navyapi || \
+  die "Could not start navyapi"
+
+echo "Starting navystreamwatcher"
+erl docker run -d --name navystreamwatcher -p 4041:4041 navyproject/navystreamwatcher || \
+  die "Could not start navystreamwatcher"
